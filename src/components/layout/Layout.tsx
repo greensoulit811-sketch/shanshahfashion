c7693c3e87b38f9022e5ae1f5360220f8daa2872
Suhan| Fullstack Developer <greensoulit811@gmail.com>
2026-04-13 17:12:19 +0600

    update for db

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { Header } from './Header';
 import { Footer } from './Footer';
-import { TopBar } from './TopBar';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,7 +8,6 @@ interface LayoutProps {
 export function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen flex flex-col">
-      <TopBar />
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
